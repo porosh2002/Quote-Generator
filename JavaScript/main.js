5d161055caf3c495afee799068e981851fb9eb81
porosh2002 <jamilkashem2002@gmail.com>
2020-05-27 18:22:36 +0600

    _

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,47 +1,36 @@
-const bars = document.querySelector(".bars");
-const first_bar = document.querySelector(".bar_one")
-const middle_bar = document.querySelector(".bar_two")
-bars.addEventListener("click", () => {
-    middle_bar.classList.toggle('br_mdl');
-    first_bar.classList.toggle('br_fst');
-    document.querySelector(".navigation_nav").classList.toggle("nav_on_click")
+$(".srvc_links_nv").click(() => {
+  document.querySelector(".nav_mega").classList.toggle("nav_mega_2");
+  $(".cancel_icon").click(() => {
+    document.querySelector(".nav_mega").classList.add("nav_mega_2");
+  });
+  $(".mega_links").click(() => {
+    document.querySelector(".nav_mega").classList.add("nav_mega_2");
+  });
 });
-const footer_span = document.querySelector(".footer_span");
-var year = (new Date).getFullYear();
-footer_span.innerHTML = year;
-$('.navLinks').click(function(){
-    const e = document.querySelector(".navigation_nav")
-    e.classList.remove('nav_on_click')
-    t = document.querySelector(".bar_one");
-    o = document.querySelector(".bar_two");
-    t.classList.remove("br_fst");
-    o.classList.remove("br_mdl");
-  });
-  window.addEventListener("scroll", () => {
-    const e = document.querySelector(".nav_sec");
-    window.scrollY > 90 && (e.style.backgroundColor = "#fff");
-    if(window.scrollY >120){
-      e.classList.add('nav_shadow')
-    }
-    if(window.scrollY <120){
-      e.classList.remove('nav_shadow')
-    }
-  });
-  new Glide('.glide',{
-    type: 'carousel',
-    startAt: 0,
-    perView: 4,
-    autoplay:3000,
-    breakpoints: {
-      800: {
-        perView: 3
-      },
-      600:{
-        perView:2
-      }, 
-       400:{
-        perView:1
-      }
-    }
-    
-  }).mount()
+window.addEventListener("scroll", () => {
+  const e = document.querySelector(".nav_sec");
+  window.scrollY > 90 && (e.style.backgroundColor = "#fff");
+  if (window.scrollY > 120) {
+    e.classList.add("nav_shadow");
+  }
+  if (window.scrollY < 120) {
+    e.classList.remove("nav_shadow");
+  }
+});
+new Glide(".glide", {
+  type: "carousel",
+  startAt: 0,
+  perView: 4,
+  autoplay: 3000,
+  breakpoints: {
+    800: {
+      perView: 3,
+    },
+    600: {
+      perView: 2,
+    },
+    400: {
+      perView: 1,
+    },
+  },
+}).mount();
